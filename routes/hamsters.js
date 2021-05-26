8fc7dde56e43073abe58cd28b8b531fa45c0495a
nicole pena fuentes <nicole.pena.fuentes@iths.se>
2021-05-26 11:09:13 +0200

    POST and DELETE route

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -49,7 +49,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
 	const object = req.body
 
-	if ( !object || !object.name || object.age ) {
+	if ( !isHamsterObject(object) ) {
 		res.sendStatus(400)
 		return
 	}
@@ -62,9 +62,45 @@ router.post('/', async (req, res) => {
 
 // PUT /hamsters/:id
 
+router.put ('/:id', async (req, res) => {
+	const object = req.body
+	const id = req.params.id
+
+	if( !object || !id) {
+		res.sendStatus(400)
+		return
+	}
+	const docRef = db.collection('Hamsters').doc(id)
+	await docRef.set(object, { merge: true}) 
+	res.sendStatus(200)
+})
+
+function isHamsterObject(maybeObject) {
+	if ( !maybeObject ) 
+		return false
+	else if ( !maybeObject.name || maybeObject.age )
+ 		return true
+	}
+
 
 //DELETE /hamstesrs/:id
 
+router.delete('/:id', async (req, res) => {
+	const id = req.params.id
+
+	if ( !id ) {
+		res.sendStatus(400)
+		return
+	}
+
+	const result = await db.collection('Hamsters').doc(id).delete()
+	res.sendStatus(200)
+	if ( !docRef.exists ) {
+		res.status(404).send('hamster does not exist').get()
+		return
+	}
+
+})
 
 
 
