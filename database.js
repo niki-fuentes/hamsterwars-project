7f21fabfb1ca8132591101f7dc02ea8e0db6ab16
nicole pena fuentes <nicole.pena.fuentes@iths.se>
2022-04-03 17:36:42 +0200

    fixing errors that came with deploy

--- database.js
+++ database.js
@@ -2,16 +2,23 @@ const admin = require("firebase-admin");
 
 
 
-let serviceAccount;
+let privateKey;
 if( process.env.PRIVATE_KEY ) {
-	serviceAccount = JSON.parse(process.env.PRIVATE_KEY);
+	privateKey = JSON.parse(process.env.PRIVATE_KEY)
 } else {
-	serviceAccount = require("./hamster-private-key.json");
+	privateKey = require('./private-key-animals.json')
 }
 
+
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+
+credential: admin.credential.cert(serviceAccount),
+
 });
+
+
+
 function getDatabase() {
 
 return admin.firestore();
