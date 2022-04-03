ffa932a1dfefee06253d3e113984bda110879471
nicole pena fuentes <nicole.pena.fuentes@iths.se>
2022-04-03 17:14:31 +0200

    fixes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 app.use( express. json() )
 app.use( cors() )
 app.use( express.static(staticFolder) )
-
+app.use('/img', express.static(imgFolder) );
 
 
 //REST API för HAMSTERS
